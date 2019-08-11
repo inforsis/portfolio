9597a060a4e23b56f86f93a015a60c7b7748b541
inforsis <inforsis@gmail.com>
2019-08-11 00:08:18 -0300

    menu via json

--- html/js/main.js
+++ html/js/main.js
@@ -1,6 +1,7 @@
 import { CountUp } from './countUp.min.js';
 
 window.onload = function() {
+  loadMenu();
   loadCoverArea();
   //alert(window.innerWidth);
 }
@@ -348,6 +349,33 @@ window.loadStatistics = function() {
 
     loadCountNumber();
     
+  })
+}
+
+function loadMenu() {
+  axios.get('./json/menu.json')
+  .then(function(response){
+    let obj = (response.data);
+    var menu = document.getElementById('mainMenu');
+    menu.innerHTML = "";
+    for (let i in obj) {
+      let item = document.createElement('A');
+      if (i == 0)
+        item.classList.add('active');
+      item.classList.add('main-menu-item');
+      item.setAttribute('href',obj[i].router);      
+      item.setAttribute('id',obj[i].id);      
+      item.setAttribute('onclick','navMainMenu(this.id);'+obj[i].callback);      
+      item.setAttribute('data-title',obj[i].title); 
+      
+      let icon = document.createElement('I');
+      icon.classList.add('material-icons');
+      icon.innerHTML = obj[i].icon;
+
+      item.appendChild(icon);
+
+      menu.appendChild(item);
+    }
   })
 }
 
