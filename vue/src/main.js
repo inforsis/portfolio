d86a5a770338480b37a721154317b71b26e86d73
Jadson Moreira <inforsis@gmail.com>
2020-04-14 01:20:03 -0300

    adicionado componente vue-gtag para adicionar o analytics, atualizado background da home e exportado para formato otimizado

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
+import VueGtag from 'vue-gtag'
 import Router from 'vue-router'
 
-Vue.use(Router);
+Vue.use(Router)
 import App from './App.vue'
 import routes from './router'
+
+Vue.use(VueGtag, {
+  config: {
+    id: "UA-2477848-15"
+  }
+}, routes)
 
 Vue.config.productionTip = false
 
